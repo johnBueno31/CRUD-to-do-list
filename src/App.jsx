ad6e09dfdf35da4db8cb24ba4fdbe9934dd79900
John <johnranierbueno@gmail.com>
2024-05-16 10:51:44 -0700

    full functionality to do list

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+function App() {
+	const [todos, setTodos] = useState([]);
+	const [todoValue, setTodoValue] = useState("");
+
+	function handleAddTodos(newTodo) {
+		const newTodoList = [...todos, newTodo];
+		setTodos(newTodoList);
+	}
+
+	function handleDeleteTodo(index) {
+		const newTodoList = todos.filter((todo, todoIndex) => {
+			return todoIndex !== index;
+		});
+
+		setTodos(newTodoList);
+	}
+
+	function handleEditTodo(index) {
+		const valueToBeEdited = todos[index];
+		setTodoValue(valueToBeEdited);
+		handleDeleteTodo(index);
+	}
+	return (
+		<>
+			<TodoInput
+				todoValue={todoValue}
+				setTodoValue={setTodoValue}
+				handleAddTodos={handleAddTodos}
+			/>
+			<TodoList
+				handleEditTodo={handleEditTodo}
+				handleDeleteTodo={handleDeleteTodo}
+				todos={todos}
+			/>
+		</>
+	);
+}
+
+export default App;
